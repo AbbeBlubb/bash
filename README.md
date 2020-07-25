d50c13d2063f22d89808042244fc1b98d96d5f2b
Alberto Chávez Pérez <alberto@francisco.nu>
2020-07-25 22:59:07 +0200

    Script deletes '00 ' from filenames

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# bash
+# bash scripts
+
+Scripts written for private tasks
---
+++ rename.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+## Delete "00 " from filenames in folder.
+# When downloaded my music files from Google Music, lot's of files starded with "00 <filename>"
+
+for file in *.mp3; do
+	if [[ $file == 00[[:space:]]* ]]
+	then
+		mv "$file" "${file#00 }"
+		echo -e "Renamed from: $file\n        to:      ${file#00 }";
+	else
+		echo "$file didn't match test"
+	fi 
+done
+
+
+# mv "$file" "${file%.txt}x.txt"
+# Hash inside expansion.
+# http://wiki.bash-hackers.org/syntax/pe#from_the_end
+
+# mv "$file" "${file#00 }.txt"
+# % inside expansion.
+# https://wiki.bash-hackers.org/syntax/pe#from_the_beginning
